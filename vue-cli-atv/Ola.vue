bb88b577eea1d833de77d09a09fb4e611896a50f
rubem-dias <rubemdias65@gmail.com>
2021-01-10 08:45:34 -0300

    parte2

---
+++ vue-cli-atv/Ola.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1> {{ titulo }} </h1>
+        <input type="text" class="v-model">
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+                titulo: 'Ola vue'
+            }
+        }
+    }
+</script>
