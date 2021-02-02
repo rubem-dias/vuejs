dc5de985043ab561c49f3e261a6b88c6d2e027a0
rubem-dias <rubemdias65@gmail.com>
2021-02-02 16:26:23 -0300

    vue-req

---
+++ vue-http/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="app">
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4"> Requisicoes HTTP </h1>
+                <p class="lead"> Usando bibilioteca axios para fazer chamadas ajax a uma API REST </p>
+            </div>
+        </div>
+
+        <div class="container">
+        </div>
+    </div>
+</template>
